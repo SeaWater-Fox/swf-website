198bfc26d0cf0a7655160d7fec15de67c26f7d13
Dylan McAllister <dmcall774@nt17008.local>
2015-05-24 00:31:29 -0400

    Migrating website from VPS

---
+++ indev/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--
+      No favicon... yet.
+      <link rel="icon" href="../../favicon.ico">
+    -->
+
+    <title>SeaWater Fox</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css?ver=1" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/cover.css?ver=1.1" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">SeaWater Fox</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">In Construction</h1>
+            <p class="lead">SeaWater Fox is an upcoming technology-orientated business that specializes in creating a better tomorrow through diverse and intricate software. More information is coming soon.</p>
+            <p class="lead">
+              <a href="#openModal" class="btn btn-lg btn-default">Join the wait</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>&copy; SeaWater Fox 2015.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="openModal" class="modalDialog">
+      <div>
+        <a href="#close" title="Close" class="close">X</a>
+        <h2 style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif">Subscribe to Our Mailing List</h2>
+        <!-- Begin MailChimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+          #mc_embed_signup{clear:left; font:14px "Helvetica Neue",Helvetica,Arial,sans-serif; }
+          #mc-embedded-subscribe{background-color:#000 !important;}
+          /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+          We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+        <div id="mc_embed_signup">
+        <form action="//seawaterfox.us10.list-manage.com/subscribe/post?u=984d3ee8dc5cd7123018f9293&amp;id=7a9fe3fea3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+          <div id="mc_embed_signup_scroll">
+  
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group">
+              <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+              </label>
+              <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+            </div>
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;"><input type="text" name="b_984d3ee8dc5cd7123018f9293_7a9fe3fea3" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          </div>
+        </form>
+      </div>
+
+<!--End mc_embed_signup-->
+  </div>
+</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	/* Redirect browser to inconstruction page */
+	header("Location: https://www.seawaterfox.com/indev/");
+ 
+	/* exit script after redirect */
+	exit;
+?>
+ 
