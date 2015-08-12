b532dee37d0f97d0443f04aa5eca7d910dd60207
Dylan McAllister <mcallister.dylan@gmail.com>
2015-08-12 16:50:25 -0400

    whoops lol

--- seawaterfox.com/indev/index.php
+++ seawaterfox.com/indev/index.php
@@ -106,7 +106,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="./js/ie10-viewport-bug-workaround.js"></script>
   </body>
